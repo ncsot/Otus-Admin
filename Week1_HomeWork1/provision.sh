9d7806e3c9e71a8bf9c720ad2c9ae52e659bbc5b
ncsot <ncsot@ya.ru>
2019-11-03 21:03:59 +0300

    Update Vagrantfile
    add Provision config to Vagrantfile

---
+++ Week1_HomeWork1/provision.sh
@@ -0,0 +1,6 @@
+sudo yum -y update
+sudo yum -y groupinstall "Development-Tools"
+sudo yum -y install ncurses-devel
+whereis bison gcc flex
+yum -y install  https://centos7.iuscommunity.org/ius-release.rpm
+yum install -y tmux2u
