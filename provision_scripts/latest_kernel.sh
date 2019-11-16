0c3fc1c57379759f89ff5c520651f20822b41830
ncsot <ncsot@ya.ru>
2019-11-16 16:43:45 +0300

    Update latest_kernel.sh

--- provision_scripts/latest_kernel.sh
+++ provision_scripts/latest_kernel.sh
@@ -4,3 +4,10 @@ LATEST_LINK=$(wget --output-document - --quiet https://www.kernel.org/ | grep -A
 wget $LATEST_LINK &> /dev/null
 
 tar -Jxvf *"$LATEST_STABLE"* -C /usr/src
+/usr/src/linux-"$LATEST_STABLE"/make mrproper
+wget https://github.com/ncsot/Otus-Admin/blob/master/Week1_HomeWork1/.config -P /usr/src/linux-"$LATEST_STABLE"
+/usr/src/linux-"$LATEST_STABLE"/make bzImage
+/usr/src/linux-"$LATEST_STABLE"/make modules
+/usr/src/linux-"$LATEST_STABLE"/make
+/usr/src/linux-"$LATEST_STABLE"/make install modules_install
+/usr/src/linux-"$LATEST_STABLE"/make install
