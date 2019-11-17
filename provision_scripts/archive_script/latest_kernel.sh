6e0db081739909dc72223de34ae4990e29a7b6f8
ncsot <ncsot@ya.ru>
2019-11-17 22:58:38 +0300

    upg kernel script

--- provision_scripts/archive_script/latest_kernel.sh
+++ provision_scripts/archive_script/latest_kernel.sh
@@ -3,6 +3,7 @@ LATEST_STABLE=(`curl -s https://www.kernel.org/ | grep -A1 'stable:' | grep -oP 
 
 LATEST_LINK=$(wget --output-document - --quiet https://www.kernel.org/ | grep -A 1 "latest_link" | grep -o -P '(?<=a href=").*(?=">)')
 
+wget "$LATEST_LINK" -P /home/vagrant/
 
 
 
